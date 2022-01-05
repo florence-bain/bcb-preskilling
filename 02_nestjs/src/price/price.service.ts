079b3581673f05273bf5946d0ba79c60a0b55bdf
Florence Bain <florence_bain@outlook.com>
2022-01-05 10:34:47 +0000

    'customised crypto api'

--- 02_nestjs/src/price/price.service.ts
+++ 02_nestjs/src/price/price.service.ts
@@ -5,24 +5,35 @@ import { lastValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class PriceService {
-  static API_ROOT = "https://min-api.cryptocompare.com/data/price";
+  static API_ROOT =
+    'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,XRP,LTC,BCH,ETC&tsyms=USD,GBP,EUR,JPY,ZAR';
 
-  constructor(private configService: ConfigService, private http: HttpService) { }
+  constructor(
+    private configService: ConfigService,
+    private http: HttpService,
+  ) {}
 
   async getPrice(from: string, to: string): Promise<number> {
     let json = await this.request<{ [key: string]: number }>(
-      PriceService.API_ROOT, {
+      PriceService.API_ROOT,
+      {
         params: {
           apiKey: this.configService.get<string>('CRYPTOCOMPARE_API_KEY'),
           fsym: from,
           tsyms: to,
-        }
-      });
+        },
+      },
+    );
     return json[to];
   }
 
-  private async request<T>(url: string, params: { [key: string]: any }): Promise<T> {
-    let request = this.http.get(url, params).pipe(map(response => response.data));
+  private async request<T>(
+    url: string,
+    params: { [key: string]: any },
+  ): Promise<T> {
+    let request = this.http
+      .get(url, params)
+      .pipe(map((response) => response.data));
     return lastValueFrom(request);
   }
 }
